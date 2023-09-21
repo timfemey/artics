425229a1dc4040626f82cf472fe76f092f7665d1
femi <isholaobafemi@gmail.com>
2023-09-21 16:51:35 +0100

    config: firebase config

---
+++ server/config/firebase.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"context"
+	"log"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/storage"
+)
+
+func runInit() *firebase.App {
+	app, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("error initializing firebase app: %v\n", err)
+	}
+	return app
+}
+
+func Firestore() *firestore.Client {
+	firebaseApp := runInit()
+	firestore, err := firebaseApp.Firestore(context.Background())
+	if err != nil {
+		log.Fatalf("error initializing firestore app: %v\n", err)
+	}
+	return firestore
+
+}
+
+func Storage() *storage.Client {
+	firebaseApp := runInit()
+	storage, err := firebaseApp.Storage(context.Background())
+	if err != nil {
+		log.Fatalf("error initializing storage app: %v\n", err)
+	}
+	return storage
+}
