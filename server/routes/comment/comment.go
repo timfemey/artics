0c7d1900e694f632712f32f8efdf5c5f95482582
femi <isholaobafemi@gmail.com>
2023-09-23 18:11:04 +0100

    patch: comment route functionality

--- server/routes/comment/comment.go
+++ server/routes/comment/comment.go
@@ -1,7 +1,41 @@
 package comment
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"artics-server/config"
+	"context"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+var firestore = config.Firestore()
+
+var auth = config.Auth()
 
 func Comment(fiber *fiber.Ctx) error {
+	ctx := context.Background()
+	docID := fiber.FormValue("id")
+	uid := fiber.FormValue("uid")
+	comment := fiber.FormValue("comment")
+
+	if len(docID) < 3 && 3 > len(comment) {
+		return fiber.Status(http.StatusForbidden).JSON(map[string]any{
+			"status":  false,
+			"message": "Article not found, Comment not sent",
+		})
+	}
+
+	_, err := auth.GetUser(ctx, uid)
+	if err != nil {
+		return fiber.Status(http.StatusForbidden).JSON(map[string]any{
+			"status":  false,
+			"message": "Invalid Account, Create an account before uploading a comment",
+		})
+	}
+
+	firestore.Collection("article").Doc(docID).Collection("comments").Add(ctx, map[string]any{
+		"uid":     uid,
+		"comment": comment,
+	})
 	return nil
 }
